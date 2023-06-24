62a77213e5c84e53c2b5514e3fd6fba8780486cf
roaming97 <roaming98a@gmail.com>
2023-06-24 11:48:07 -0600

    Add svelte-awesome-pug

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ My custom [SvelteKit](https://kit.svelte.dev/) template.
 ## Features
 
 -   Pug
+-   Svelte Awesome Pug
 -   TypeScript
 -   SASS
 -   Vite
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
+import { awesomePugPre, awesomePugPost } from 'svelte-awesome-pug'
 import preprocess from 'svelte-preprocess'
 import auto from '@sveltejs/adapter-auto'
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	extensions: ['.svelte'],
-	preprocess: [preprocess()],
+	preprocess: [awesomePugPre, preprocess(), awesomePugPost],
 	kit: {
 		adapter: auto()
 	}
